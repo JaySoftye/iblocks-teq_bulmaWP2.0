5430d8c9671db8a366fec90849c3a3314e3d4411
JaySoftye <jonathansoftye@teq.com>
2021-04-27 16:51:54 -0400

    new template addded
    compiled list of iBlock Pathways in test-content.php template added

--- inc/test-content.php
+++ inc/test-content.php
@@ -7,82 +7,89 @@
 ?>
 
 
-<div class="page-section full-width-mobile iblocks-impact"><div class="container is-fluid fade-in-container"><div class="columns is-vcentered">
-<div class="column is-two-fifths padding-sm orange-background"><div class="centered-container has-text-right target-animate animated fadeInUp">
-<h1 class="extra-large-header white-text with-more-icon">Why<br>iBlocks?</h1>
-</div></div>
-<div class="column padding-sm target-animate-slow animated fadeInUp">
-<h4 class="floating-text white-text bottom">future<br>marine<br>biologists</h4>
-</div>
-</div></div></div>
-<div class="after-section-margin"></div>
+<div class="green-background" style="height:100vh;display:flex;align-items:flex-end;padding-bottom:5vh;">
+  <div class="container">
+  <div class="columns card rounded-corners drop-shadow nomargin hidden">
+    <div class="column is-full nopadding rounded-corners">
 
-<div class="section full-height background full-width-mobile fade-in-container nopadding">
-<figure style="width: 63%;" class="top right">
-  <img src="/wp-content/uploads/2021/04/iblock-impact-wave1.svg" />
-</figure>
-<figure style="width: 100%; z-index:100;" class="bottom">
-  <img class="bottom" src="/wp-content/uploads/2021/04/iblock-impact-wave2.svg" />
-</figure>
-<figure style="width: 21%;" class="top right">
-  <img src="/wp-content/uploads/2021/04/iblock-impact-image1.svg" />
-</figure>
-<div class="columns is-vcentered nomargin">
-<div class="column is-two-fifths full-height background-cover iblocks-impact-student-impact"></div>
-<div class="column is-three-fifths"><div class="padding">
-<h2 class="sub-header strong orange-text">Unforgettable career-based exploration </h2>
-<p class="padding-bottom">An iBlock is a <strong>10-part sequence</strong> of student-led, teacher-guided lessons and activities that center on technology and <strong>culminate in a capstone project.</strong><br /><br />With iBlocks, students get to practice future-ready skills in an organic way, as they explore the topic of their iBlock project. Designing a snorkel and then 3D printing it to see how it works? Engineering a prosthetic flipper and then testing its functionality? Imagining a deep-sea dive to collect data on marine life? From the ocean floor all the way to outer space — there’s <strong>no end to the possibilities</strong> of experiential learning!</p>
-</div></div>
-</div>
-</div>
-<div class="section full-height background full-width-mobile fade-in-container nopadding">
-<figure style="width:100%; z-index:100;" class="top">
-  <img src="/wp-content/uploads/2021/04/iblock-impact-wave3.svg" />
-</figure>
-<figure style="width:100%; bottom:0; z-index:100;">
-  <img class="bottom" src="/wp-content/uploads/2021/04/iblock-impact-wave4.svg" />
-</figure>
-<figure style="width:10.2%; z-index:100; top:50%; left: 55%;">
-  <img class="bottom" src="/wp-content/uploads/2021/04/iblock-impact-image4.svg" />
-</figure>
-<div class="columns is-vcentered nomargin">
-<div class="column is-three-fifths"><div class="padding has-text-right">
-<h2 class="sub-header strong orange-text">This is the iBlocks mission, should you choose to accept it: </h2>
-<p>Students will lead the way, while the teacher will offer guidance and coaching. There will be trial and error, testing and re-testing, and maybe even some dead ends. But those dead ends will spark new ideas, and those new ideas will mean <strong>innovation, creativity, and lessons learned.</strong> The student result is always success.</p>
-<br />
-<p class="strong orange-text">A holistic, cross-curricular approach </p>
-<p class="is-size-5">iBlocks are designed to be flexible and adapt to the learning that’s taking place. Because they’re cross-curricular and skills-based, they also offer numerous benefits for schools: </p>
-<br />
-<p class="is-size-6">
-<span>• Project- and problem-based learning<span><br />
-<span>• Practical applications of 21st century skills<span><br />
-<span>• Practicing the Engineering Design Process<span><br />
-<span>• Career/industry exploration<span><br />
-<span>• Social and emotional learning (SEL)<span><br />
-<span>• Advanced literacies<span><br />
-<span>• Standards alignment<span><br />
-<span>• Customizable learning content<span><br />
-<span>• Option to incorporate STEM products of your choice<span><br />
-<span>• Anytime, anywhere learning<span><br />
-</p>
-</div></div>
-<div class="column is-two-fifths full-height background-cover iblocks-impact-school-benefit"></div>
-</div>
-</div>
+      <div class="table-container iblock-pathways-reference-table">
+        <table class="table is-hoverable">
+          <thead>
+            <th></th>
+            <th>Elementary School</th>
+            <th>Middle School</th>
+            <th>High School</th>
+            <th>Coding</th>
+            <th>ELA</th>
+            <th>Engineering</th>
+            <th>Hydroponics</th>
+            <th>Mathematics</th>
+            <th>Robotics</th>
+            <th>Science</th>
+            <th>Social Studies</th>
+            <th>Special Education</th>
+            <th>Agriculture</th>
+            <th>Applied Science</th>
+            <th>Arts</th>
+            <th>Business</th>
+            <th>Energy</th>
+            <th>Environment</th>
+            <th>Finance</th>
+            <th>Health</th>
+            <th>Math/CS</th>
+            <th>Transportation</th>
+          </thead>
+          <tbody>
 
-<div class="section white-background-fill full-width-mobile nopadding">
-<figure style="width:100%; top:0; z-index:100;">
-<img class="bottom" src="/wp-content/uploads/2021/04/iblock-impact-wave5.svg" />
-  </figure>
-<div class="container is-fluid padding-bottom"><div class="columns is-vcentered padding-top padding-bottom">
-<div class="column is-three-fifths">
-<p class="has-text-right strong orange-text">Get started with your career-based exploration!</p>
-<p class="has-text-right">Your iBlock includes everything you need to implement it effectively in the classroom, from a framework that aligns to national standards, to student workbooks, a teacher’s guide to help you facilitate, and even self-assessments to help students keep their learning on track.</p>
-</div>
-<div class="column">
-<p class="has-text-centered"><a class="button half-width contact-us-modal-button"  data-target="modal-ter" aria-haspopup="true">Contact us for more info</a></p>
-</div>
-</div></div>
-</div>
+            <?php
+              $args = array(
+                'post_type' => 'stateiblocks',
+                'category_name' => 'iblock-pathways',
+                'posts_per_page' => -1,
+                'orderby' => 'title',
+                'order'   => 'ASC',
+              );
+              $the_query = new WP_Query($args);
+                if ( $wp_query->have_posts() ) :
+                  while ($the_query -> have_posts()) :
+                    $the_query -> the_post();
+            ?>
+            <tr>
+              <td>
+                <a href="<?php echo get_post_meta( $post->ID, 'custom_url_meta_content', true ); ?>">
+                  <?php the_title(); ?>
+                </a>
+              </td>
+              <td <?php if ( in_category('elementary-school') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('middle-school') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('high-school') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('coding') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('ela') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('engineering') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('Hydroponics') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('math') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('robotics') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('science') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('social-studies') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('special-education') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('agriculture') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('applied-science') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('arts') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('business') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('energy') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('environment') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('finance') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('health') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('math-cs') ) { echo 'class="checked"'; } ?>></td>
+              <td <?php if ( in_category('transportation') ) { echo 'class="checked"'; } ?>></td>
+            </tr>
+            <?php endwhile; endif; ?>
 
-<?php include( get_template_directory() . '/inc/Prosthetic-hand-iblock-download-form.php'); ?>
+          </tbody>
+        </table>
+      </div>
+
+    </div>
+  </div>
+  </div>
+</div>
